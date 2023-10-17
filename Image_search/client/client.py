c419860bff782c58931ef45b0db9f5da3da21282
Dhushyanth <125176193+dhushyanth-h-m@users.noreply.github.com>
2023-10-17 23:19:45 +0000

    build a docker container

--- Image_search/client/client.py
+++ Image_search/client/client.py
@@ -7,14 +7,14 @@ channel = grpc.insecure_channel('localhost:3080', options=[('grpc.max_receive_me
 
 def run():
     # Connect to the gRPC server
-    with grpc.insecure_channel('localhost:50051') as channel:
+    with grpc.insecure_channel('localhost:3080') as channel:
         stub = image_search_pb2_grpc.ImageSearchServiceStub(channel)
 
         # Prompt the user to enter a query
-        # query = input('Enter a query: ')
+        query = input('Enter a query: ')
 
         # Send a request to search for an image
-        response = stub.SearchImage(image_search_pb2.ImageRequest(keyword="dog"))
+        response = stub.SearchImage(image_search_pb2.ImageRequest(keyword=query))
 
         # Save the image to a file
         with open('image.jpg', 'wb') as image_file:
--- model.py
+++
@@ -1,7 +0,0 @@
-import keras
-
-# Download the VGG16 model
-model = keras.applications.vgg16.VGG16(weights='imagenet')
-
-# Save the model to a file
-model.save('model.h5')
